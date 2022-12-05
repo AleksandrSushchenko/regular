80338bf29c75534d036bbc4f20b9c8807105abe5
Aleksander Sushchenko <039pro@gmail.com>
2022-12-05 20:18:55 +0200

    create word name

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import re
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+with open("phonebook_raw.csv", 'r', encoding='utf-8') as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+    # new_list = []
+# print(contacts_list[1])
+
+for i in contacts_list:
+    lfs = re.findall('[А-Я][а-я]+'," ".join(i))
+    print(lfs[0:3])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

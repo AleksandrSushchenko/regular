25a4c9d661fedaecd31642fe9da78b008f5599cf
AleksandrSushchenko <039pro@gmail.com>
2022-12-06 12:15:11 +0200

    Заработали имена

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import csv
 with open("phonebook_raw.csv", 'r', encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-    new_list = []
 
 #
 # for i in contacts_list[1:]:
@@ -14,62 +13,26 @@ with open("phonebook_raw.csv", 'r', encoding='utf-8') as f:
 def fio():
     serch_fio = r'([А-Я][а-я]+)'
     zamena_fio = r' \1'
-    for column in contacts_list[1:]:
-        line = column[0] + column[1] + column[2]
-        if len((re.sub(serch_fio, zamena_fio, line).split())) == 3:
-            column[0] = re.sub(serch_fio, zamena_fio, line).split()[0]
-            column[1] = re.sub(serch_fio, zamena_fio, line).split()[1]
-            column[2] = re.sub(serch_fio, zamena_fio, line).split()[2]
-        elif len((re.sub(serch_fio, zamena_fio, line).split())) == 2:
-            column[0] = re.sub(serch_fio, zamena_fio, line).split()[0]
-            column[1] = re.sub(serch_fio, zamena_fio, line).split()[1]
-            column[2] = ''
-        elif len((re.sub(serch_fio, zamena_fio, line).split())) == 1:
-            column[0] = re.sub(serch_fio, zamena_fio, line).split()[0]
-            column[1] = ''
-            column[2] = ''
+    for stolb in contacts_list[1:]:
+        stroka = stolb[0] + stolb[1] + stolb[2]
+        if len((re.sub(serch_fio, zamena_fio, stroka).split())) == 3:
+            stolb[0] = re.sub(serch_fio, zamena_fio, stroka).split()[0]
+            stolb[1] = re.sub(serch_fio, zamena_fio, stroka).split()[1]
+            stolb[2] = re.sub(serch_fio, zamena_fio, stroka).split()[2]
+        elif len((re.sub(serch_fio, zamena_fio, stroka).split())) == 2:
+            stolb[0] = re.sub(serch_fio, zamena_fio, stroka).split()[0]
+            stolb[1] = re.sub(serch_fio, zamena_fio, stroka).split()[1]
+            stolb[2] = ''
     return
 
-
-def phone_number_formatting():
-    phone_pattern = re.compile(
-        r'(\+7|8)?\s*\(?(\d{3})\)?\s*\D?(\d{3})[-\s+]?(\d{2})-?(\d{2})((\s)?\(?(доб.)?\s?(\d+)\)?)?')
-    phone_substitution = r'+7(\2)\3-\4-\5\7\8\9'
-    for column in contacts_list:
-        column[5] = phone_pattern.sub(phone_substitution, column[5])
-    return
-
-
-def duplicates_combining():
-    for column in contacts_list[1:]:
-        first_name = column[0]
-        last_name = column[1]
-        for contact in contacts_list:
-            new_first_name = contact[0]
-            new_last_name = contact[1]
-            if first_name == new_first_name and last_name == new_last_name:
-                if column[2] == '':
-                    column[2] = contact[2]
-                if column[3] == '':
-                    column[3] = contact[3]
-                if column[4] == '':
-                    column[4] = contact[4]
-                if column[5] == '':
-                    column[5] = contact[5]
-                if column[6] == '':
-                    column[6] = contact[6]
-
-    for contact in contacts_list:
-        if contact not in new_list:
-            new_list.append(contact)
-    return new_list
-
+def phone_format():
+    pass
 
 if __name__ == '__main__':
     fio()
-    phone_number_formatting()
-    duplicates_combining()
+    print(contacts_list)
+
 
     with open("phonebook.csv", "w", encoding='utf-8') as f:
         datawriter = csv.writer(f, delimiter=',')
-        datawriter.writerows(new_list)
+        datawriter.writerows(contacts_list)

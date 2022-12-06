fd0557b591fdd26485e5a69419dd8a9ea251a773
AleksandrSushchenko <039pro@gmail.com>
2022-12-06 14:51:49 +0200

    Заработали имена Порпавлены телефоны доделал

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import csv
 with open("phonebook_raw.csv", 'r', encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-
+new_list=[]
 #
 # for i in contacts_list[1:]:
-#     lfs = re.findall('[А-Я][а-я]+'," ".join(i))
-#     print(lfs[0:3])
+#      lfs = re.findall('[А-Я][а-я]+'," ".join(i))
+#      print(lfs[0:2])
 
 def fio():
     serch_fio = r'([А-Я][а-я]+)'
@@ -32,10 +32,32 @@ def phone_format():
         stolb[5] = phone_search.sub(phone_zamena, stolb[5])
     return
 
+def duble_search():
+    for stolb in contacts_list[1:]:
+        first_name = stolb[0]
+        last_name = stolb[1]
+        for people in contacts_list:
+            new_first_name = people[0]
+            new_last_name = people[1]
+            if first_name == new_first_name and last_name == new_last_name:
+                if stolb[2] == '':
+                    stolb[2] = people[2]
+                if stolb[3] == '':
+                    stolb[3] = people[3]
+                if stolb[4] == '':
+                    stolb[4] = people[4]
+                if stolb[5] == '':
+                    stolb[5] = people[5]
+                if stolb[6] == '':
+                    stolb[6] = people[6]
+
+
+
 if __name__ == '__main__':
     fio()
     phone_format()
-    print(contacts_list)
+    duble_search()
+
 
 
     with open("phonebook.csv", "w", encoding='utf-8') as f:

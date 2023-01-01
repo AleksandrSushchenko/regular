e0b5efc04d00f48139d0ddcc9ab46d03e37f233c
Aleksander Sushchenko <039pro@gmail.com>
2023-01-01 22:58:32 +0200

    исправление

--- main.py
+++ main.py
@@ -51,6 +51,11 @@ def duble_search():
                 if stolb[6] == '':
                     stolb[6] = people[6]
 
+    for contact in contacts_list:
+        if contact not in new_list:
+            new_list.append(contact)
+    return new_list
+
 
 
 if __name__ == '__main__':
